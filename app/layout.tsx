26b49437c8ea864901f952b1b6d297c539d59c69
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-04-16 12:31:45 +0200

    feat: page layout init (navbar, footer)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Script from 'next/script';
+
+import '~/globals.sass';
+
 export const metadata = {
     metadataBase: new URL('https://nove.team'),
     viewport: {
@@ -7,7 +13,6 @@ export const metadata = {
     },
     title: 'Live. Laugh. Nove.',
     description: 'An unexpected focus on privacy with all standard features or even more. Surprising emphasis on data security. Enjoy everything for free and open-source to everyone.',
-    image: '/cdn/assets/banner.png',
     category: 'technology',
     assets: ['/cdn/assets'],
     colorScheme: 'dark',
@@ -73,7 +78,109 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <Script id="border-nav" strategy="afterInteractive">
+                    {`const el = document.querySelector(".navBox");
+                    const observer = new IntersectionObserver(([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1), { threshold: [1] });
+                    observer.observe(el);`}
+                </Script>
+                <nav className="navBox">
+                    <div className="container">
+                        <Link href="/">
+                            <header>
+                                <Image src="/cdn/assets/logo.png" width={32} height={32} alt="Logo grayscale" />
+                                <h1>Nove</h1>
+                            </header>
+                        </Link>
+                        <ul>
+                            <li>
+                                <Link href="/">Projects</Link>
+                            </li>
+                            <li>
+                                <Link href="/">Docs</Link>
+                            </li>
+                            <li>
+                                <Link href="/">About</Link>
+                            </li>
+                            <li>
+                                <Link href="/">GitHub</Link>
+                            </li>
+                            <li>
+                                <Link href="/">Changelog</Link>
+                            </li>
+                            <li>
+                                <Link href="/">Open source</Link>
+                            </li>
+                            <li>
+                                <Link href="/">Legal</Link>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li className="login">
+                                <Link href="/">Login</Link>
+                            </li>
+                            <li className="button">
+                                <Link href="/">Sign up</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                {children}
+
+                <footer className="footerBox">
+                    <div className="container">
+                        <aside>
+                            <header>
+                                <div className="card">
+                                    <Image src="/cdn/assets/logo.png" width={32} height={32} alt="Logo grayscale" />
+                                    <h1>Nove</h1>
+                                </div>
+                                <p>&copy; 2020-{new Date().getFullYear()} Nove Group.</p>
+                            </header>
+                        </aside>
+                        <section className="content">
+                            <div className="card">
+                                <header>Product</header>
+                                <ul>
+                                    <li>
+                                        <Link href="/">Changelog</Link>
+                                    </li>
+                                    <li>
+                                        <a href="https://beta.cheems.dog">Cheems</a>
+                                    </li>
+                                    <li>
+                                        <Link href="/">Docs</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/">Developers</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="card">
+                                <header>Company</header>
+                                <ul>
+                                    <li>
+                                        <Link href="/">About</Link>
+                                    </li>
+                                    <li>
+                                        <a href="https://github.com/orgs/nove-org/discussions">Community</a>
+                                    </li>
+                                    <li>
+                                        <Link href="/">Open source</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/">Privacy Policy</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/">Terms of Service</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </section>
+                    </div>
+                </footer>
+            </body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <main></main>;
+    return (
+        <main>
+            <h1>Live. Laugh. Nove</h1>
+        </main>
+    );
 }
