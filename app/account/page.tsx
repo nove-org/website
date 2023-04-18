175fc59ed1979e8e99055cc2abb3d0d7197b503c
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-04-18 05:27:32 +0000

    feat: dashboard overview page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,12 +1,24 @@
 'use client';
 
+import Image from 'next/image';
 import o from '~/account/page.module.sass';
 
 export default function Privacy() {
     return (
         <div className={o.content}>
             <h1 className={o.title}>Overview</h1>
-            <div className={o.card}></div>
+            <div className={o.card}>
+                <label htmlFor="image">
+                    <Image src="https://api.nove.team/v1/users/00000000/avatar.webp" width={96} height={96} alt="User avatar" />
+                </label>
+                <div className={o.content}>
+                    <div className={o.username}>
+                        <h1>wnm210</h1>
+                        <button>Edit</button>
+                    </div>
+                    <div className={o.email}>wnm210@****.team</div>
+                </div>
+            </div>
             <div className={o.connections}></div>
         </div>
     );
