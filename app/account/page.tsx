604eb04913162e3137fd944417ed8e058c22898f
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-04-18 07:36:54 +0200

    feat: added connections init

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import o from '~/account/page.module.sass';
 
-export default function Privacy() {
+export default function AccountOverview() {
     return (
         <div className={o.content}>
             <h1 className={o.title}>Overview</h1>
@@ -19,7 +19,19 @@ export default function Privacy() {
                     <div className={o.email}>wnm210@****.team</div>
                 </div>
             </div>
-            <div className={o.connections}></div>
+            <div className={o.connections}>
+                <h2>Account connections</h2>
+                <div className={o.card}>
+                    <div className={o.info}>
+                    <Image src="https://api.nove.team/v1/users/00000000/avatar.webp" width={96} height={96} alt="User avatar" />
+                    <header>
+                        <h1>cheems.dog</h1>
+                        <span>Basic user permissions</span>
+                    </header>
+                    </div>
+                    <button>Deauthorize</button>
+                </div>
+            </div>
         </div>
     );
 }
