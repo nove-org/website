efe645692ee6531e2762e8fb84a8ce2e1f7d0a62
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-04-27 17:42:36 +0200

    feat: popups on security page

--- app/account/security/page.tsx
+++ app/account/security/page.tsx
@@ -1,11 +1,63 @@
 'use client';
 
+import { useState } from 'react';
 import o from '~/account/page.module.sass';
 import Device from './device';
 
 export default function AccountSecurity() {
+    const [passwordPopup, setPasswordPopup] = useState<boolean>(false);
+    const [emailPopup, setEmailPopup] = useState<boolean>(false);
+
     return (
         <div className={o.content}>
+            {passwordPopup ? (
+                <dialog id="changeName" className={o.popup}>
+                    <div onClick={() => setPasswordPopup(false)} className={o.background}></div>
+                    <form autoComplete="off">
+                        <h1>
+                            Change your password
+                            <svg onClick={() => setPasswordPopup(false)} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                                <path
+                                    fill="currentColor"
+                                    d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path>
+                            </svg>
+                        </h1>
+                        <p>Choose a strong password and do not reuse it for other accounts. Changing your password will sign you out on your devices.</p>
+                        <input autoComplete="off" autoFocus={true} autoCorrect="off" type="text" placeholder="Verify your old password" id="oldPassword" name="oldPassword" />
+                        <input autoComplete="off" autoFocus={false} autoCorrect="off" type="text" placeholder="New password" id="accountPasswordUpdate" name="accountPasswordUpdate" />
+                        <div className={o.footer}>
+                            <button onClick={() => setPasswordPopup(false)} type="reset">
+                                Cancel
+                            </button>
+                            <button type="submit">Save changes</button>
+                        </div>
+                    </form>
+                </dialog>
+            ) : null}
+            {emailPopup ? (
+                <dialog id="changeName" className={o.popup}>
+                    <div onClick={() => setEmailPopup(false)} className={o.background}></div>
+                    <form autoComplete="off">
+                        <h1>
+                            Recovery email
+                            <svg onClick={() => setEmailPopup(false)} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                                <path
+                                    fill="currentColor"
+                                    d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path>
+                            </svg>
+                        </h1>
+                        <p>The address where we can contact you if there is an unusual activity in your account or if you get locked out.</p>
+                        <input autoComplete="off" autoFocus={true} autoCorrect="off" type="text" placeholder="Password" id="password" name="password" />
+                        <input autoComplete="off" autoFocus={false} autoCorrect="off" type="text" placeholder="New recovery email" id="accountRecoveryEmail" name="accountRecoveryEmail" />
+                        <div className={o.footer}>
+                            <button onClick={() => setEmailPopup(false)} type="reset">
+                                Cancel
+                            </button>
+                            <button type="submit">Save changes</button>
+                        </div>
+                    </form>
+                </dialog>
+            ) : null}
             <h1 className={o.title}>Security</h1>
             <div className={o.security}>
                 <div className={o.card}>
@@ -21,15 +73,7 @@ export default function AccountSecurity() {
                 <div className={o.card}>
                     <header>How do you sign in</header>
                     <p>Add more secure ways of signing in to your account and confirming your identity</p>
-                    <div className={o.option}>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
-                            <path
-                                fill="currentColor"
-                                d="M21.694,15.28L21.694,15.28c-0.399-0.399-1.05-0.389-1.436,0.024l-1.708,1.832L17.414,16l1.293-1.293 c0.39-0.39,0.39-1.023,0-1.413l-0.001-0.001c-0.39-0.39-1.023-0.39-1.413,0L16,14.586l-3.791-3.792l0.21-0.44 c0.914-1.913,0.676-4.249-0.783-5.788C9.453,2.264,5.617,2.512,3.789,5.31C2.887,6.69,2.738,8.49,3.412,9.995 c1.212,2.708,4.29,3.689,6.745,2.518l0.637-0.305l8.499,8.499c0.39,0.39,1.023,0.39,1.413,0l0.001-0.001 c0.39-0.39,0.39-1.023,0-1.413l-0.744-0.744l1.754-1.88C22.085,16.276,22.075,15.661,21.694,15.28z M10.127,10.127 c-1.17,1.17-3.073,1.17-4.243,0c-1.17-1.17-1.17-3.073,0-4.243c1.17-1.17,3.073-1.17,4.243,0 C11.296,7.054,11.296,8.957,10.127,10.127z"></path>
-                        </svg>
-                        Add two factor authentication
-                    </div>
-                    <div className={o.option}>
+                    <div onClick={() => setPasswordPopup(true)} className={o.option}>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
                             <path
                                 fill="currentColor"
@@ -37,7 +81,15 @@ export default function AccountSecurity() {
                         </svg>
                         Password
                     </div>
-                    <div className={o.option}>
+                    <div onClick={() => setEmailPopup(true)} className={o.option}>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                            <path
+                                fill="currentColor"
+                                d="M20,4H4C2.895,4,2,4.895,2,6v12c0,1.105,0.895,2,2,2h16c1.105,0,2-0.895,2-2V6C22,4.895,21.105,4,20,4z M19.601,8.249 l-7.071,4.42c-0.324,0.203-0.736,0.203-1.06,0l-7.071-4.42C4.151,8.094,4,7.822,4,7.53v0c0-0.666,0.733-1.072,1.297-0.719L12,11 l6.703-4.189C19.267,6.458,20,6.864,20,7.53v0C20,7.822,19.849,8.094,19.601,8.249z"></path>
+                        </svg>
+                        Recovery email
+                    </div>
+                    <div className={o.option + ' disabled'}>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
                             <path
                                 fill="currentColor"
@@ -45,13 +97,13 @@ export default function AccountSecurity() {
                         </svg>
                         Recovery passphrase
                     </div>
-                    <div className={o.option}>
+                    <div className={o.option + ' disabled'}>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
                             <path
                                 fill="currentColor"
-                                d="M20,4H4C2.895,4,2,4.895,2,6v12c0,1.105,0.895,2,2,2h16c1.105,0,2-0.895,2-2V6C22,4.895,21.105,4,20,4z M19.601,8.249 l-7.071,4.42c-0.324,0.203-0.736,0.203-1.06,0l-7.071-4.42C4.151,8.094,4,7.822,4,7.53v0c0-0.666,0.733-1.072,1.297-0.719L12,11 l6.703-4.189C19.267,6.458,20,6.864,20,7.53v0C20,7.822,19.849,8.094,19.601,8.249z"></path>
+                                d="M21.694,15.28L21.694,15.28c-0.399-0.399-1.05-0.389-1.436,0.024l-1.708,1.832L17.414,16l1.293-1.293 c0.39-0.39,0.39-1.023,0-1.413l-0.001-0.001c-0.39-0.39-1.023-0.39-1.413,0L16,14.586l-3.791-3.792l0.21-0.44 c0.914-1.913,0.676-4.249-0.783-5.788C9.453,2.264,5.617,2.512,3.789,5.31C2.887,6.69,2.738,8.49,3.412,9.995 c1.212,2.708,4.29,3.689,6.745,2.518l0.637-0.305l8.499,8.499c0.39,0.39,1.023,0.39,1.413,0l0.001-0.001 c0.39-0.39,0.39-1.023,0-1.413l-0.744-0.744l1.754-1.88C22.085,16.276,22.075,15.661,21.694,15.28z M10.127,10.127 c-1.17,1.17-3.073,1.17-4.243,0c-1.17-1.17-1.17-3.073,0-4.243c1.17-1.17,3.073-1.17,4.243,0 C11.296,7.054,11.296,8.957,10.127,10.127z"></path>
                         </svg>
-                        Recovery email
+                        Add two factor authentication
                     </div>
                 </div>
             </div>
