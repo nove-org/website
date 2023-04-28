d1955e407aeb392e5bf06364b2adff13420c2c5f
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-04-28 20:19:40 +0200

    feat: basic account info page

--- app/account/personal/page.tsx
+++ app/account/personal/page.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 
 import o from '~/account/page.module.sass';
+import u from '~/account/personal.module.sass';
 
 export default function AccountPersonal() {
     return (
@@ -10,20 +11,30 @@ export default function AccountPersonal() {
             <h1 className={o.title}>Personal info</h1>
             <p className={o.description}>Manage your personal info displayed on your Nove account profile. You can change display method of your profile to private and public. While on private profile we will share only basic info about your account like username and avatar.</p>
 
-            <div className={o.profile}>
-                <div className={o.card}>
+            <div className={u.profile}>
+                <div className={u.card}>
                     <header>Basic account info</header>
-                    <div className={o.input}>
-                        <header>Username</header>
-                        <div className={o.content}>
-                            <h1>wnm210</h1>
-                            <span>Edit</span>
+                    <div className={u.input}>
+                        <header>Avatar</header>
+                        <div className={u.content}>
+                            <Image src="https://api.nove.team/v1/users/00000000/avatar.webp" width={64} height={64} alt="Avatar" />
+                            <a>Edit</a>
                         </div>
                     </div>
-                    <div className={o.input}>
-                        <header>Avatar</header>
-                        <div className={o.content}>
-                            <Image src="/cdn/assets/avatar.png" width={64} height={64} alt="Avatar" />
+                    <div className={u.input}>
+                        <header>Username</header>
+                        <div className={u.content}>
+                            <h1>wnm210</h1>
+                            <a>Edit</a>
+                        </div>
+                    </div>
+                </div>
+                <div className={u.card}>
+                    <header>Details</header>
+                    <div className={u.input}>
+                        <header>Bio</header>
+                        <div className={u.content}>
+                            <textarea id="bio" name="bio" rows={7} placeholder="Start typing to save..." />
                         </div>
                     </div>
                 </div>
--- app/account/security/page.tsx
+++ app/account/security/page.tsx
@@ -41,16 +41,16 @@ export default function AccountSecurity() {
                     <div onClick={() => setEmailPopup(false)} className={o.background}></div>
                     <form autoComplete="off">
                         <h1>
-                            Recovery email
+                            Change your email
                             <svg onClick={() => setEmailPopup(false)} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
                                 <path
                                     fill="currentColor"
                                     d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path>
                             </svg>
                         </h1>
-                        <p>The address where we can contact you if there is an unusual activity in your account or if you get locked out.</p>
+                        <p>The address where we can contact you if there is an unusual activity in your account or if you get locked out. You can also use it to log in to your Nove account.</p>
                         <input autoComplete="off" autoFocus={true} autoCorrect="off" type="text" placeholder="Password" id="password" name="password" />
-                        <input autoComplete="off" autoFocus={false} autoCorrect="off" type="text" placeholder="New recovery email" id="accountRecoveryEmail" name="accountRecoveryEmail" />
+                        <input autoComplete="off" autoFocus={false} autoCorrect="off" type="text" placeholder="New contact email" id="accountRecoveryEmail" name="accountRecoveryEmail" />
                         <div className={o.footer}>
                             <button onClick={() => setEmailPopup(false)} type="reset">
                                 Cancel
@@ -84,6 +84,14 @@ export default function AccountSecurity() {
                         Password
                     </div>
                     <div onClick={() => setEmailPopup(true)} className={s.option}>
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                            <path
+                                fill="currentColor"
+                                d="M20,4H4C2.895,4,2,4.895,2,6v12c0,1.105,0.895,2,2,2h16c1.105,0,2-0.895,2-2V6C22,4.895,21.105,4,20,4z M19.601,8.249 l-7.071,4.42c-0.324,0.203-0.736,0.203-1.06,0l-7.071-4.42C4.151,8.094,4,7.822,4,7.53v0c0-0.666,0.733-1.072,1.297-0.719L12,11 l6.703-4.189C19.267,6.458,20,6.864,20,7.53v0C20,7.822,19.849,8.094,19.601,8.249z"></path>
+                        </svg>
+                        Email
+                    </div>
+                    <div onClick={() => setEmailPopup(true)} className={s.option + ' disabled'}>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
                             <path
                                 fill="currentColor"
