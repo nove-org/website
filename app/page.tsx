cf366027f1a4fb111f86273d73535f4db1daac9e
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-08-10 12:21:59 +0200

    fix: comment syntax highlighting

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,8 @@
  GNU Affero General Public License for more details.
 
  You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 import { axiosClient } from '@util/axios';
 import o from '@sass/page.module.sass';
