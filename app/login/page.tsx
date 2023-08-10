b6ad526e2656e5e3feb87a5e36a0b58ca688ce40
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-08-10 12:59:51 +0200

    fix: lines while loading login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,7 @@ import { getCookie, setCookie } from 'cookies-next';
 import { useEffect, useState } from 'react';
 import { COOKIE_HOSTNAME } from '@util/config';
 import Loader from '@app/Loader';
+import Lines from './Lines';
 
 export default function Login() {
     const [loading, setLoading] = useState<boolean>(true);
@@ -37,27 +38,25 @@ export default function Login() {
         const user = await axiosClient
             .post('/v1/users/login', { username: form.get('username'), password: form.get('password') })
             .then((user) => {
-                if (user?.data?.body?.error) return throwError(user.data.body.error.message);
-                else {
-                    setCookie('napiAuthorizationToken', user.data.body.data.token, {
-                        maxAge: 3 * 30 * 24 * 60 * 60,
-                        domain: COOKIE_HOSTNAME,
-                        sameSite: 'strict',
-                        secure: true,
-                    });
+                setCookie('napiAuthorizationToken', user.data.body.data.token, {
+                    maxAge: 3 * 30 * 24 * 60 * 60,
+                    domain: COOKIE_HOSTNAME,
+                    sameSite: 'strict',
+                    secure: true,
+                });
 
-                    const uri = searchParams.get('redirectBack') || '/account';
+                const uri = searchParams.get('redirectBack') || '/account';
 
-                    if (uri == '__CLOSE__') window.close();
-                    else window.location.replace(uri);
-                }
+                if (uri == '__CLOSE__') window.close();
+                else window.location.replace(uri);
             })
             .catch((err) => (err?.response?.data?.body?.error ? throwError(err.response.data.body.error.message) : console.error(err)));
     };
 
     return loading ? (
         <section className={o.box}>
-            <Loader type="window" text="Please standby while fetching data..." />
+            <Loader type="window" text="Please standby while we fetch the data..." />
+            <Lines />
         </section>
     ) : (
         <section className={o.box}>
@@ -84,44 +83,7 @@ export default function Login() {
             <a className={o.passwordReset} href="/password-reset">
                 Forgot your password?
             </a>
-            <svg className={o.left} width="369" height="421" viewBox="0 0 369 421" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clipPath="url(#clip0_206_257)">
-                    <path d="M-17.1175 99.5959C486.128 -180.012 209.998 253.278 376.332 431.615" stroke="url(#paint0_linear_206_257)" strokeWidth="3" />
-                    <path d="M-23.1142 82.9993C407.075 -176.883 185.568 252.003 341.819 443.071" stroke="url(#paint1_linear_206_257)" strokeWidth="3" />
-                </g>
-                <defs>
-                    <linearGradient id="paint0_linear_206_257" x1="124.053" y1="30.5978" x2="268.983" y2="467.246" gradientUnits="userSpaceOnUse">
-                        <stop stopColor="#DB4B53" />
-                        <stop offset="1" stopColor="#EF1973" />
-                    </linearGradient>
-                    <linearGradient id="paint1_linear_206_257" x1="98.0528" y1="19.751" x2="248.806" y2="473.943" gradientUnits="userSpaceOnUse">
-                        <stop stopColor="#DB4B53" />
-                        <stop offset="1" stopColor="#EF1973" />
-                    </linearGradient>
-                    <clipPath id="clip0_206_257">
-                        <rect width="369" height="421" fill="white" />
-                    </clipPath>
-                </defs>
-            </svg>
-            <svg className={o.right} width="485" height="437" viewBox="0 0 485 437" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clipPath="url(#clip0_206_264)">
-                    <path d="M486.073 295.863C138.591 679.277 220.501 165.191 0.439427 -1.89633" stroke="url(#paint0_linear_206_264)" strokeWidth="3" />
-                    <path d="M533.034 289.497C188.361 677.116 261.747 150.266 35.8579 -25.5479" stroke="url(#paint1_linear_206_264)" strokeWidth="3" />
-                </g>
-                <defs>
-                    <linearGradient id="paint0_linear_206_264" x1="384.93" y1="393.177" x2="83.8931" y2="-57.6251" gradientUnits="userSpaceOnUse">
-                        <stop stopColor="#FF007A" />
-                        <stop offset="1" stopColor="#FF003D" />
-                    </linearGradient>
-                    <linearGradient id="paint1_linear_206_264" x1="432.443" y1="387.638" x2="119.312" y2="-81.2766" gradientUnits="userSpaceOnUse">
-                        <stop stopColor="#FF007A" />
-                        <stop offset="1" stopColor="#FF003D" />
-                    </linearGradient>
-                    <clipPath id="clip0_206_264">
-                        <rect width="485" height="437" fill="white" />
-                    </clipPath>
-                </defs>
-            </svg>
+            <Lines />
         </section>
     );
 }
