fe571e8c9aaf5023fd9e060a8d28cb4d102b9fc9
wnm210 <49320100+wnm210@users.noreply.github.com>
2023-08-12 10:44:17 +0200

    feat(accountProfile): styling for username form

--- app/account/profile/Username.tsx
+++ app/account/profile/Username.tsx
@@ -3,17 +3,29 @@
 import { axiosClient } from '@util/axios';
 import { User } from '@util/schema';
 import { useState } from 'react';
+import o from '@sass/account/profile/page.module.sass';
 
 export default function Username({ user, cookie }: { user: User; cookie?: string }) {
     const [edit, setEdit] = useState<boolean>(false);
+    const [postError, setPostError] = useState<string>();
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
+    const throwError = (message?: string, bool?: boolean) => {
+        if (bool === false) return setPostError('');
 
+        if (message) {
+            setPostError(message.charAt(0).toUpperCase() + message.slice(1).toLowerCase());
+
+            setTimeout(() => setPostError(''), 4000);
+        }
+    };
+
+    const handleSubmit = async (e: any) => (
+        e.preventDefault(),
         await axiosClient
             .patch('/v1/users/me', { username: e.target.username.value }, { headers: { Authorization: `Owner ${cookie}` } })
-            .then(() => (setEdit(false), window.location.reload()));
-    };
+            .then(() => (setEdit(false), window.location.reload()))
+            .catch((e) => throwError(e.response?.data.body?.error.message ? e.response.data.body.error.message : 'Something went wrong and we cannot explain it.'))
+    );
 
     return (
         <>
@@ -28,8 +40,9 @@ export default function Username({ user, cookie }: { user: User; cookie?: string
                     </>
                 ) : (
                     <form onSubmit={handleSubmit}>
-                        <input type="text" placeholder={user.username} name="username" required />
+                        <input type="text" placeholder={user.username + ' (new username here)'} name="username" required />
                         <button type="submit">Save changes</button>
+                        {postError ? <p className={o.error}>{postError}</p> : null}
                     </form>
                 )}
             </li>
