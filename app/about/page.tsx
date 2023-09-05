e1b447a161f7478030e55faedfa77c7c2c97f3d2
wnm210 <matt@nove.team>
2023-09-05 17:14:46 +0200

    chore: role update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,14 +6,14 @@ export default function About() {
         <section className={o.hero}>
             <title>About — Nove</title>
             <h1 className={o.title}>
-                Our <b>goal</b> is to make the Internet <span>cleaner</span> and <span>safer</span>.
+                Our <b>goal</b> is to make the Internet <span>more private</span> and <span>safer</span>.
             </h1>
             <p className={o.desc}>We provide tools free from bloatware, trackers, and annoying advertisements. With free &amp; open-source policy.</p>
             <ul>
                 <li>
                     <Image src="https://api.nove.team/v1/users/b9dk4Pdm/avatar.webp" width="64" height="64" alt="Avatar" />
                     <h1>JuzioMiecio520</h1>
-                    <p>CEO and back-end developer</p>
+                    <p>CEO and senior back-end developer</p>
                 </li>
                 <li>
                     <Image src="https://api.nove.team/v1/users/00000000/avatar.webp" width="64" height="64" alt="Avatar" />
@@ -23,7 +23,7 @@ export default function About() {
                 <li>
                     <Image src="https://api.nove.team/v1/users/00000001/avatar.webp" width="64" height="64" alt="Avatar" />
                     <h1>Dawid</h1>
-                    <p>Back-end developer, code maintainer</p>
+                    <p>Main back-end developer, code maintainer</p>
                 </li>
                 <li>
                     <Image src="https://api.nove.team/v1/users/00000002/avatar.webp" width="64" height="64" alt="Avatar" />
