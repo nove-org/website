a2359a2e98bee566431053baa377170adc0148b3
wnm210 <matt@nove.team>
2023-09-19 16:30:52 +0200

    fix: privacy policy corrections by @SlaVistaPL https://git.nove.team/nove-org/website/pulls/22#issuecomment-1104

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -25,19 +25,18 @@ export default function Terms() {
                 Last modified: <time>19th of September 2023</time>
             </h2>
             <p>
-                We serve (mostly) free and open-source services. We do not track nor sell user&apos;s data to third-parties. We keep our services simple, without requests to Google
-                or Facebook. We do not use tracking pixel or fingerprinting. We do not profile our user&apos;s.
+                We serve (mostly) free and open-source services. We do not track nor sell user&apos;s data to third-parties. We do not use any forms of tracking pixels or generate
+                a unique fingerprint about you.
             </p>
+            <p>We keep the right to update this Privacy Policy without notifying users about the change.</p>
+            <p>By using our services you agree to this document.</p>
+            <h2>Infrastructure</h2>
             <p>
-                To delete your account go to Security section on the Account page, then scroll down to &quot;Delete Nove account&quot;. All your data will be deleted as fast as
-                possible and as smooth as possible. This will change in the future.
-            </p>
-            <p>
-                FTDL provide for us server infrastructure (located in Krakow, Poland) so we can make our services accessible to anyone. You can read about them on their{' '}
+                We host our services on the infrastructure provided by FTDL (
                 <a href="https://ftdl.pl/services/" rel="noopener noreferrer" target="_blank">
                     website
                 </a>
-                . Administrator of your data is Nove (<a href="mailto:reply@nove.team">reply@nove.team</a>)
+                ) which is located in Krakow, Poland. Your data administrator is Nove (nove.team; <a href="mailto:reply@nove.team">reply@nove.team</a>)
             </p>
             <p>In our database, we only store necessary info you provided us through NAPI. Detailing</p>
             <ul>
@@ -58,8 +57,12 @@ export default function Terms() {
                 me is not displayed if you change your profile to private.
             </p>
             <p>Provided email is not used for marketing purposes, we only use it to confirm your identity, notify you about new login or to confirm email change</p>
-            <p>We keep the right to update this Privacy Policy without notifying users about the change.</p>
-            <p>By using our services you agree to this document.</p>
+
+            <h2>Data retention policy</h2>
+            <p>
+                To delete your account go to Security section on the Account page, then scroll down to &quot;Delete Nove account&quot;. All your data will be deleted as fast as
+                possible and as smooth as possible.
+            </p>
             <p>Data you have encrypted in our services using your password is truly encrypted and we cannot decrypt it.</p>
             <p>We can delete accounts if they are not verified and inactive for at least 3 days.</p>
             <p>We can delete accounts for violation of our Terms of Service</p>
