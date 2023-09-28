2381cf442c78aa454ad3a830f0824b3c867bcdb4
wnm210 <matt@nove.team>
2023-09-28 23:27:25 +0200

    feat(LH): about page translations

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,8 @@
 import o from '@sass/about.module.sass';
 import Image from 'next/image';
+import LanguageHandler from '@util/handlers/LanguageHandler';
+import { axiosClient } from '@util/axios';
+import { cookies } from 'next/headers';
 
 export const metadata = {
     title: 'Nove | About',
@@ -17,34 +20,48 @@ export const metadata = {
     keywords: ['nove', 'about nove', 'about'],
 };
 
-export default function About() {
+export default async function About() {
+    const user = await axiosClient
+        .get('/v1/users/me', {
+            headers: { Authorization: `Owner ${cookies()?.get('napiAuthorizationToken')?.value}` },
+        })
+        .catch((e) => e.response);
+
+    const lang = await new LanguageHandler('main/about', user.data.body.data).init();
+
     return (
         <section className={o.hero}>
-            <title>Nove | About</title>
+            <title>{`Nove | ${lang.getProp('title')}`}</title>
             <h1 className={o.title}>
-                Our <b>goal</b> is to make the Internet <span>more private</span> and <span>safer</span>.
+                {lang.getProp('hero-h1')} <b>{lang.getProp('hero-h1-span1')}</b> {lang.getProp('hero-h1-next1')} <span>{lang.getProp('hero-h1-span2')}</span>{' '}
+                {lang.getProp('hero-h1-next2')} <span>{lang.getProp('hero-h1-span3')}</span>.
             </h1>
-            <p className={o.desc}>We provide tools free from bloatware, trackers, and annoying advertisements. With free &amp; open-source policy.</p>
+            <p className={o.desc}>{lang.getProp('hero-p')}</p>
             <ul>
                 <li>
                     <Image src="https://api.nove.team/v1/users/00000000/avatar.webp" width="64" height="64" alt="Avatar" />
                     <h1>wnm210</h1>
-                    <p>CEO, front-end developer and UI/UX designer</p>
+                    <p>{lang.getProp('pos1')}</p>
                 </li>
                 <li>
                     <Image src="https://api.nove.team/v1/users/00000001/avatar.webp" width="64" height="64" alt="Avatar" />
                     <h1>Dawid</h1>
-                    <p>Main back-end developer, code maintainer</p>
+                    <p>{lang.getProp('pos2')}</p>
                 </li>
                 <li>
                     <Image src="https://api.nove.team/v1/users/00000002/avatar.webp" width="64" height="64" alt="Avatar" />
                     <h1>ArsBeneMoriendi</h1>
-                    <p>UI/UX designer and project manager</p>
+                    <p>{lang.getProp('pos3')}</p>
+                </li>
+                <li>
+                    <Image src="https://api.nove.team/v1/users/OzcH9li3/avatar.webp" width="64" height="64" alt="Avatar" />
+                    <h1>SlavistaPL</h1>
+                    <p>{lang.getProp('pos4')}</p>
                 </li>
                 <li>
                     <Image src="https://api.nove.team/v1/users/b9dk4Pdm/avatar.webp" width="64" height="64" alt="Avatar" />
                     <h1>JuzioMiecio520</h1>
-                    <p>Retired back-end developer</p>
+                    <p>{lang.getProp('pos5')}</p>
                 </li>
             </ul>
         </section>
