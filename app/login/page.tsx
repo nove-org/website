c83f24a8839e1b5bee5c709cb352f6fbec9fd948
wnm210 <matt@nove.team>
2023-09-29 18:05:05 +0200

    refactor: login page server-side rendered

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,89 +1,56 @@
-'use client';
-
 import { axiosClient } from '@util/axios';
-import { useSearchParams } from 'next/navigation';
 import Logo from '../Logo';
 import o from '@sass/login.module.sass';
-import { getCookie, setCookie } from 'cookies-next';
-import { useEffect, useState } from 'react';
-import { COOKIE_HOSTNAME } from '@util/config';
-import Loader from '@app/Loader';
 import Lines from './Lines';
+import LanguageHandler from '@util/handlers/LanguageHandler';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
+import LoginForm from './Form';
 
-export default function Login() {
-    const [loading, setLoading] = useState<boolean>(true);
-    const [postError, setPostError] = useState<string>();
-    const searchParams = useSearchParams();
+export const metadata = {
+    title: 'Nove | Login',
+    openGraph: {
+        title: 'Nove | Login',
+        images: [],
+    },
+    twitter: {
+        title: 'Nove | Login',
+        images: [],
+    },
+    keywords: ['nove', 'nove login', 'about'],
+};
 
-    const throwError = (message?: string, bool?: boolean) => {
-        if (bool === false) return setPostError('');
+export default async function Login({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
+    const redirectBack: string | undefined = searchParams['redirectBack'];
+    const user = await axiosClient
+        .get('/v1/users/me', {
+            headers: { Authorization: `Owner ${cookies()?.get('napiAuthorizationToken')?.value}` },
+        })
+        .catch((e) => e.response);
 
-        if (message) {
-            setPostError(message.charAt(0).toUpperCase() + message.slice(1).toLowerCase());
+    if (user?.data?.body?.data?.username) return redirect(redirectBack ? redirectBack : '/account');
 
-            setTimeout(() => setPostError(''), 5000);
-        }
-    };
+    const browserLanguage: string | undefined = headers().get('Accept-Language')?.split(',')[0];
+    const lang = await new LanguageHandler('main/login', user.data.body.data).init(browserLanguage);
 
-    useEffect(() => {
-        const fetchUser = async () =>
-            await axiosClient
-                .get('/v1/users/me', { headers: { Authorization: `Owner ${getCookie('napiAuthorizationToken')}` } })
-                .then((user) => (user.data?.body?.data?.username ? window.location.replace(searchParams.get('redirectBack') || '/account') : setLoading(false)))
-                .catch(() => setLoading(false));
-        fetchUser();
-    }, [searchParams]);
-
-    const handleLogin = async (form: FormData) => {
-        const user = await axiosClient
-            .post('/v1/users/login', { username: form.get('username'), password: form.get('password') })
-            .then((user) => {
-                setCookie('napiAuthorizationToken', user.data.body.data.token, {
-                    maxAge: 3 * 30 * 24 * 60 * 60,
-                    domain: COOKIE_HOSTNAME,
-                    sameSite: 'strict',
-                    secure: true,
-                });
-
-                const uri = searchParams.get('redirectBack') || '/account';
-
-                if (uri == '__CLOSE__') window.close();
-                else window.location.replace(uri);
-            })
-            .catch((err) => (err?.response?.data?.body?.error ? throwError(err.response.data.body.error.message) : console.error(err)));
-    };
-
-    return loading ? (
+    return (
         <section className={o.box}>
-            <title>Nove | Login</title>
-            <Loader type="window" text="Please standby while we fetch the data..." />
-            <Lines />
-        </section>
-    ) : (
-        <section className={o.box}>
-            <title>Nove | Login</title>
+            <title>{`Nove | ${lang.getProp('title')}`}</title>
             <Logo size={48} />
-            <h1>Welcome back</h1>
-            <p>Provide your credentials to access your Nove account</p>
-            <form id="loginForm" action={handleLogin}>
-                <label htmlFor="username">Login</label>
-                <input type="text" id="username" name="username" />
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
-                <div className={o.flex}>
-                    <button type="submit">
-                        Proceed{' '}
-                        <svg className={o.button} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="12" height="12" viewBox="0 0 50 50">
-                            <path
-                                fill="currentColor"
-                                d="M 14.988281 3.992188 C 14.582031 3.992188 14.21875 4.238281 14.0625 4.613281 C 13.910156 4.992188 14 5.421875 14.292969 5.707031 L 33.585938 25 L 14.292969 44.292969 C 14.03125 44.542969 13.925781 44.917969 14.019531 45.265625 C 14.109375 45.617188 14.382813 45.890625 14.734375 45.980469 C 15.082031 46.074219 15.457031 45.96875 15.707031 45.707031 L 35.707031 25.707031 C 36.097656 25.316406 36.097656 24.683594 35.707031 24.292969 L 15.707031 4.292969 C 15.519531 4.097656 15.261719 3.992188 14.988281 3.992188 Z"></path>
-                        </svg>
-                    </button>
-                    {postError ? <p className="error">{postError}</p> : null}
-                </div>
-            </form>
+            <h1>{lang.getProp('hero-h1')}</h1>
+            <p>{lang.getProp('hero-p')}</p>
+            <LoginForm
+                lang={{
+                    inputLogin: lang.getProp('input-login'),
+                    inputPassword: lang.getProp('input-password'),
+                    inputBtn: lang.getProp('input-btn'),
+                }}
+                user={user?.data?.body?.data}
+                searchParam={redirectBack}
+            />
             <a className={o.passwordReset} href="/password-reset">
-                Forgot your password?
+                {lang.getProp('password-reset')}
             </a>
             <Lines />
         </section>
