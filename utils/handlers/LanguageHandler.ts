3ec5e01ba82aec46c8a4c54c43d06e0da7ac0071
wnm210 <matt@nove.team>
2023-09-29 17:53:03 +0200

    feat(LH): implement variables and language based on browser settings

--- utils/handlers/LanguageHandler.ts
+++ utils/handlers/LanguageHandler.ts
@@ -2,21 +2,26 @@ import { readFileSync } from 'fs';
 import { jsonc } from 'jsonc';
 import { User } from '@util/schema';
 import ObjectHelper from '@util/helpers/Object';
+import { browser } from 'process';
 
 export default class LanguageHandler {
     private name: string;
     private category: string;
-    private language: string;
+    private language: string | undefined;
     private file: any;
     private props: string | undefined;
 
     constructor(path: string, user: User) {
         this.category = path.split('/')[0];
         this.name = path.split('/')[1];
-        this.language = user?.language || 'en-US';
+        this.language = user?.language;
     }
 
-    public async init() {
+    public async init(browserLanguage?: string | undefined) {
+        if (!browserLanguage) browserLanguage = 'en-US';
+        if (!browserLanguage.split('-')[1] && !this.language) this.language = `${browserLanguage}-${browserLanguage.toUpperCase()}`;
+        else if (!this.language) this.language = browserLanguage;
+
         const file = jsonc.parse(readFileSync(`./languages/${this.language}.jsonc`).toString());
 
         if (!file) throw new ReferenceError(`LanguageHandler: Error while parsing ${this.language}.jsonc`);
@@ -33,10 +38,15 @@ export default class LanguageHandler {
         return this;
     }
 
-    public getProp(path: string) {
-        const propValue = ObjectHelper.getValueByStringPath(this.props, path);
+    public getProp(path: string, vars?: object) {
+        let propValue: string = ObjectHelper.getValueByStringPath(this.props, path);
 
         if (!propValue) throw new ReferenceError(`LanguageHandler: Error while parsing ${this.language}.jsonc: prop "${path}" does not exist`);
+
+        if (vars)
+            for (const [key, value] of Object.entries(vars)) {
+                propValue = propValue.replaceAll('{' + key + '}', value);
+            }
 
         return propValue;
     }
