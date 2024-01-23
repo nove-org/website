9e467dac9130e7e0fef786e3ecb84c4409ec52e6
wnm210 <matt@nove.team>
2024-01-23 20:54:13 +0100

    fix: sync language handler files

--- app/blog/BlogImage.tsx
+++ app/blog/BlogImage.tsx
@@ -7,17 +7,19 @@ import Image from 'next/image';
 export default function BlogImage({ text }: { text: string }) {
     const [postBackground, setPostBackground] = useState<{ src: string; alt: string }>({
         src: '/no_image.png',
-        alt: 'Empty image',
+        alt: 'No image',
     });
 
     useEffect(() => {
         const postDOM = document.createElement('div');
         postDOM.classList.add('postDOM');
         postDOM.innerHTML = text;
-        setPostBackground({
-            src: postDOM.getElementsByTagName('img')[0].src,
-            alt: postDOM.getElementsByTagName('img')[0].alt,
-        });
+        if (postDOM.getElementsByTagName('img')[0]) {
+            setPostBackground({
+                src: postDOM.getElementsByTagName('img')[0].src,
+                alt: postDOM.getElementsByTagName('img')[0].alt,
+            });
+        }
     }, [text]);
 
     return <Image className={o.banner} src={postBackground.src} alt={postBackground.alt} width="500" height="222" />;
