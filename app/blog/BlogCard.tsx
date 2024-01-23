c50816381764dadf0dea6d396525f020d67ed103
wnm210 <matt@nove.team>
2024-01-23 20:52:34 +0100

    fix: load blog cards with noscript

--- app/blog/BlogCard.tsx
+++ app/blog/BlogCard.tsx
@@ -1,22 +1,16 @@
-'use client';
-
 import o from '@sass/blog.module.sass';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Post } from '@util/schema';
+import BlogImage from './BlogImage';
 
 export default function BlogCard({ post }: { post: Post }) {
     const date = new Date(post.createdAt);
 
-    const postDOM = document.createElement('div');
-    postDOM.classList.add('postDOM');
-    postDOM.innerHTML = post.text;
-    const postBackground = postDOM.getElementsByTagName('img')[0];
-
     return (
         <li title={post.title} key={post.id}>
             <Link href={`/blog/${post.id}`}>
-                <Image className={o.banner} src={postBackground.src} alt={postBackground.alt} width="500" height="222" />
+                <BlogImage text={post.text} />
                 <div className={o.info}>
                     <h1>{post.title}</h1>
                     <time>Published on {date.toLocaleString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}</time>
