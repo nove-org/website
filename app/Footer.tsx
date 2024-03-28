3b15b7cd17642ecb3994373d4ec79545e00aba7d
wnm210 <matt@nove.team>
2024-03-28 18:19:47 +0100

    chore: update footer styling

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -33,69 +33,63 @@ export default function Footer({
         <>
             <div className={o.emptyMr}></div>
             <footer className={o.box + ` ${pathname.startsWith('/account') ? o.hide : ''}`}>
-                <div className={o.padding}>
-                    <header>
-                        <div className={o.title}>
-                            <Logo size={20} />
-                            Nove
-                        </div>
-                        <div className={o.copyright}>
-                            &copy; 2019-{year} {lang.license + ' '}
-                            <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" rel="noopener noreferrer" target="_blank">
-                                AGPL-3.0
-                            </a>
-                        </div>
-                        <div className={o.copyright}>
-                            {lang.made_with + ' '}
-                            <a href={REPOSITORY}>{lang.contributors}</a>
-                        </div>
-                    </header>
-                    <div className={o.links}>
-                        <ul>
-                            <li>
-                                <Link href="/about">{lang.about}</Link>
-                            </li>
-                            <li>
-                                <Link href="/blog">{lang.blog}</Link>
-                            </li>
-                            <li>
-                                <a href="https://git.nove.team/nove-org/NAPI/wiki">{lang.docs}</a>
-                            </li>
-                            <li>
-                                <a target="_blank" rel="noopener noreferrer" href={DONATE_LINK}>
-                                    {lang.donate}
-                                </a>
-                            </li>
-                            <li>
-                                <Link href="/login">{lang.login}</Link>
-                            </li>
-                            <li>
-                                <Link href="/register">{lang.register}</Link>
-                            </li>
-                            <li>
-                                <a href={'mailto:' + SUPPORT_MAIL}>{lang.support}</a>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <a target="_blank" rel="noopener noreferrer" href={REPOSITORY}>
-                                    {lang.src}
-                                </a>
-                            </li>
-                            <li>
-                                <Link href="/privacy">{lang.privacy}</Link>
-                            </li>
-                            <li>
-                                <Link href="/terms">{lang.terms}</Link>
-                            </li>
-                            <li>
-                                <Link href="/account">{lang.developers}</Link>
-                            </li>
-                            <li>
-                                <Link href="/foss">FOSS</Link>
-                            </li>
-                        </ul>
+                <header>
+                    <div className={o.title}>
+                        <Logo size={20} />
+                        Nove
                     </div>
+                    <div className={o.copyright}>
+                        &copy; 2019-{year} {lang.license + ' '}
+                        <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" rel="noopener noreferrer" target="_blank">
+                            AGPL-3.0
+                        </a>
+                    </div>
+                    <div className={o.copyright}>
+                        {lang.made_with + ' '}
+                        <a href={REPOSITORY}>{lang.contributors}</a>
+                    </div>
+                </header>
+                <div className={o.links}>
+                    <ul>
+                        <h2>General</h2>
+                        <li>
+                            <Link href="/about">{lang.about}</Link>
+                        </li>
+                        <li>
+                            <Link href="/blog">{lang.blog}</Link>
+                        </li>
+                        <li>
+                            <a target="_blank" rel="noopener noreferrer" href={DONATE_LINK}>
+                                {lang.donate}
+                            </a>
+                        </li>
+                        <li>
+                            <Link href="/login">{lang.login}</Link>
+                        </li>
+                        <li>
+                            <a href={'mailto:' + SUPPORT_MAIL}>{lang.support}</a>
+                        </li>
+                        <li>
+                            <Link href="/foss">FOSS</Link>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h2>Documents</h2>
+                        <li>
+                            <a target="_blank" rel="noopener noreferrer" href={REPOSITORY}>
+                                {lang.src}
+                            </a>
+                        </li>
+                        <li>
+                            <Link href="/privacy">{lang.privacy}</Link>
+                        </li>
+                        <li>
+                            <Link href="/terms">{lang.terms}</Link>
+                        </li>
+                        <li>
+                            <a href="https://git.nove.team/nove-org/NAPI/wiki">{lang.docs}</a>
+                        </li>
+                    </ul>
                 </div>
             </footer>
         </>
