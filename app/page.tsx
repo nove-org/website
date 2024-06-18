869427d05c40286806f26dad0556e5598b20afb6
wnm210 <matt@nove.team>
2024-06-18 09:03:32 +0200

    feat: improved why section

--- app/page.tsx
+++ app/page.tsx
@@ -183,93 +183,59 @@ export default async function Home() {
                     <h1>With all the great features you expect, and more...</h1>
                     <p>Including the modern design, intuitive user interface and accessibility options.</p>
                 </div>
-                <ul>
-                    <li>
-                        <img src="/secure.jpg" width="285" height="110" alt="Image shows how encryption works (simplified): abc <---> $2e." />
-                        <h1>{lang.getProp('word-card1-title')}</h1>
-                        <p>{lang.getProp('word-card1-description')}</p>
+                <ul className={o.whys}>
+                    <li className={o.card}>
+                        <figure className={o.encryption}>
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
+                                <path
+                                    fill="currentColor"
+                                    d="M 12 1 C 8.6761905 1 6 3.6761905 6 7 L 6 8 C 4.9 8 4 8.9 4 10 L 4 20 C 4 21.1 4.9 22 6 22 L 18 22 C 19.1 22 20 21.1 20 20 L 20 10 C 20 8.9 19.1 8 18 8 L 18 7 C 18 3.6761905 15.32381 1 12 1 z M 12 3 C 14.27619 3 16 4.7238095 16 7 L 16 8 L 8 8 L 8 7 C 8 4.7238095 9.7238095 3 12 3 z M 12 13 C 13.1 13 14 13.9 14 15 C 14 16.1 13.1 17 12 17 C 10.9 17 10 16.1 10 15 C 10 13.9 10.9 13 12 13 z"></path>
+                            </svg>
+                            <div className={o.st}>Hey! How are you?</div>
+                            <div className={o.nd}>#$*%#$*!@#&*#$%*!</div>
+                            <div className={o.rd}>!-!$.*&#=!%-#%*$#</div>
+                        </figure>
+                        <div className={o.content}>
+                            <h1>{lang.getProp('word-card1-title')}</h1>
+                            <p>{lang.getProp('word-card1-description')}</p>
+                        </div>
                     </li>
-                    <li>
-                        <img
-                            src="/no_tracking.jpg"
-                            width="285"
-                            height="110"
-                            alt="Image shows a car with tinted windows following another car at a distance. Scissors cut the road between the cars."
-                        />
-                        <h1>{lang.getProp('word-card2-title')}</h1>
-                        <p>{lang.getProp('word-card2-description')}</p>
+                    <li className={o.card}>
+                        <figure className={o.tracking}>
+                            <div className={o.cross}>
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="54" height="54" viewBox="0 0 24 24">
+                                    <path
+                                        fill="currentColor"
+                                        d="M 10 2 C 5.589 2 2 5.589 2 10 C 2 14.411 5.589 18 10 18 C 14.411 18 18 14.411 18 10 C 18 5.589 14.411 2 10 2 z M 9.7128906 4.0136719 L 9.7128906 5.0273438 C 9.3738906 5.0703438 7.4765625 5.42875 7.4765625 7.71875 C 7.4765625 11.05975 10.935547 10.069828 10.935547 12.298828 C 10.935547 13.414828 10.213406 13.441406 10.066406 13.441406 C 9.9324062 13.441406 9.0527344 13.512688 9.0527344 11.804688 L 7.1621094 11.804688 C 7.1621094 14.696687 9.2602656 14.927703 9.5722656 14.970703 L 9.5722656 15.978516 C 6.462797 15.757912 4 13.164763 4 10 C 4 6.7878034 6.5375172 4.1647918 9.7128906 4.0136719 z M 10.720703 4.0488281 C 13.69045 4.4063489 16 6.9352803 16 10 C 16 13.11536 13.614798 15.680867 10.574219 15.970703 L 10.574219 14.970703 C 10.912219 14.928703 12.824219 14.613156 12.824219 12.285156 C 12.824219 9.0801563 9.3662344 9.6935625 9.3652344 7.7265625 C 9.3652344 6.5995625 9.9851406 6.5644531 10.119141 6.5644531 C 10.364141 6.5644531 10.947266 6.7812969 10.947266 8.1542969 L 12.837891 8.1542969 C 12.837891 5.5272969 11.041703 5.1176406 10.720703 5.0566406 L 10.720703 4.0488281 z M 19.892578 8.6074219 C 19.956578 9.0644219 20 9.526 20 10 C 20 10.774 19.903328 11.523094 19.736328 12.246094 C 19.906328 12.801094 20 13.39 20 14 C 20 17.309 17.309 20 14 20 C 13.39 20 12.801094 19.906328 12.246094 19.736328 C 11.523094 19.903328 10.774 20 10 20 C 9.526 20 9.0644219 19.956578 8.6074219 19.892578 C 10.031422 21.196578 11.921 22 14 22 C 18.411 22 22 18.411 22 14 C 22 11.921 21.196578 10.031422 19.892578 8.6074219 z"></path>
+                                </svg>
+                            </div>
+                        </figure>
+                        <div className={o.content}>
+                            <h1>{lang.getProp('word-card2-title')}</h1>
+                            <p>{lang.getProp('word-card2-description')}</p>
+                        </div>
                     </li>
-                    <li>
-                        <img
-                            src="/donations.jpg"
-                            width="285"
-                            height="110"
-                            alt="Image shows two buttons. On the left a download button with an expand arrow to the right. On the right donate button with a coin icon to the right."
-                        />
-                        <h1>{lang.getProp('word-card3-title')}</h1>
-                        <p>{lang.getProp('word-card3-description')}</p>
+                    <li className={o.card}>
+                        <figure className={o.free}>
+                            <p>
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 24 24">
+                                    <path
+                                        fill="currentColor"
+                                        d="M 3.9902344 2.9902344 A 1.0001 1.0001 0 0 0 3 4.1289062 L 3 16 A 1.0001 1.0001 0 0 0 3.2929688 16.707031 L 7.2050781 20.619141 A 1.0001 1.0001 0 0 0 8.1582031 21 L 20 21 A 1.0001 1.0001 0 0 0 21 20 L 21 8.1679688 A 1.0001 1.0001 0 0 0 20.623047 7.2070312 A 1.0001 1.0001 0 0 0 20.617188 7.203125 L 16.707031 3.2929688 A 1.0001 1.0001 0 0 0 16 3 L 4.1171875 3 A 1.0001 1.0001 0 0 0 3.9902344 2.9902344 z M 6.4140625 5 L 15.585938 5 L 17.585938 7 L 8.4140625 7 L 6.4140625 5 z M 5 6.4140625 L 7 8.4140625 L 7 17.585938 L 5 15.585938 L 5 6.4140625 z M 9 9 L 19 9 L 19 19 L 9 19 L 9 9 z"></path>
+                                </svg>
+                                Compiling from source...
+                            </p>
+                            <div className={o.progress}></div>
+                        </figure>
+                        <div className={o.content}>
+                            <h1>{lang.getProp('word-card3-title')}</h1>
+                            <p>{lang.getProp('word-card3-description')}</p>
+                        </div>
                     </li>
                 </ul>
             </section>
             <section className={o.community}>
-                <aside className={o.code}>
-                    <div className={o.editor}>
-                        <div className={o.navbar}>
-                            <div className={o.file}>
-                                <img src="/go.png" width={24} alt="Golang logo" />
-                                main.go
-                            </div>
-                            <div className={o.file}>
-                                <img src="/go.png" width={24} alt="Golang logo" />
-                                api.go
-                            </div>
-                        </div>
-                        <ul className={o.lines}>
-                            <li className={o.line}>
-                                1 &nbsp;<span className={o.red}>package</span> <span className={o.white}>main</span>
-                            </li>
-                            <li className={o.line}>2</li>
-                            <li className={o.line}>
-                                3 &nbsp;<span className={o.red}>func</span> <span className={o.lightblue}>main</span>
-                                <span className={o.blue}>()</span> <span className={o.yellow}>{'{'}</span>
-                            </li>
-                            <li className={o.line}>
-                                4 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className={o.white}>name</span> <span className={o.red}>:=</span> <span className={o.white}>fmt.</span>
-                                <span className={o.lightblue}>ReadLine</span>
-                                <span className={o.blue}>()</span>
-                            </li>
-                            <li className={o.line}>
-                                5 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className={o.white}>user</span> <span className={o.red}>:=</span> <span className={o.lightblue}>GetUser</span>
-                                <span className={o.blue}>
-                                    (<span className={o.white}>name</span>)
-                                </span>
-                            </li>
-                            <li className={o.line}>6</li>
-                            <li className={o.line + ' ' + o.highlight}>
-                                7 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className={o.white}>fmt.</span>
-                                <span className={o.lightblue}>Println</span>
-                                <span className={o.blue}>(</span>
-                                <span className={o.white}>user.Id</span>
-                                <span className={o.blue}>)</span>
-                            </li>
-                            <li className={o.line + ' ' + o.highlight}>
-                                8 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className={o.white}>fmt.</span>
-                                <span className={o.lightblue}>Println</span>
-                                <span className={o.blue}>(</span>
-                                <span className={o.white}>user.FullName</span>
-                                <span className={o.blue}>)</span>
-                            </li>
-                            <li className={o.line}>
-                                9 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className={o.white}>fmt.</span>
-                                <span className={o.lightblue}>Println</span>
-                                <span className={o.blue}>(</span>
-                                <span className={o.white}>user.PermissionLevel</span>
-                                <span className={o.blue}>)</span>
-                            </li>
-                            <li className={o.line}>10 &nbsp;&nbsp;&nbsp;&nbsp;...</li>
-                        </ul>
-                    </div>
-                </aside>
+                <div></div>
                 <div className={o.text}>
                     <h1>{lang.getProp('breakup-h1')}</h1>
                     <p dangerouslySetInnerHTML={{ __html: lang.getProp('breakup-description') }} />
@@ -281,6 +247,11 @@ export default async function Home() {
                 </div>
             </section>
             <section className={o.call}>
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="64" height="64" viewBox="0 0 24 24">
+                    <path
+                        fill="currentColor"
+                        d="M 12 1 L 3 5 L 3 11 C 3 18.83 9.439 22.486 12 23 C 14.561 22.486 21 18.83 21 11 L 21 5 L 12 1 z M 12 3.1894531 L 19 6.3007812 L 19 11 C 19 12.835656 18.566184 14.395755 17.90625 15.707031 C 16.345706 14.530951 13.524154 14 12 14 C 10.475819 14 7.6533017 14.530907 6.09375 15.707031 C 5.4338159 14.395755 5 12.835656 5 11 L 5 6.3007812 L 12 3.1894531 z M 12 5.75 C 10.208 5.75 8.75 7.208 8.75 9 C 8.75 10.792 10.208 12.25 12 12.25 C 13.792 12.25 15.25 10.792 15.25 9 C 15.25 7.208 13.792 5.75 12 5.75 z M 12 7.75 C 12.689 7.75 13.25 8.311 13.25 9 C 13.25 9.689 12.689 10.25 12 10.25 C 11.311 10.25 10.75 9.689 10.75 9 C 10.75 8.311 11.311 7.75 12 7.75 z M 12 16 C 14.446733 16 15.977702 16.719961 16.908203 17.314453 C 15.340222 19.376141 13.256661 20.550006 12 20.9375 C 10.743339 20.550006 8.6597782 19.376141 7.0917969 17.314453 C 8.0222976 16.719961 9.5532669 16 12 16 z"></path>
+                </svg>
                 <h1>{lang.getProp('ready-h1')}</h1>
                 <p>{lang.getProp('ready-p')}</p>
                 <Link href="/register" className="btn">
