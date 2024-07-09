291c25407e9ce691b2d738a5a1887d6c4d19d510
wnm210 <matt@nove.team>
2024-07-09 17:17:15 +0200

    feat(profile): preview card

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -5,6 +5,7 @@ import o from './Profile.module.sass';
 import { cookies, headers } from 'next/headers';
 import Databox from '@app/Databox';
 import Image from 'next/image';
+import ReactCountryFlag from 'react-country-flag';
 
 export async function generateMetadata() {
     const api = new NAPI(cookies().get('napiAuthorizationToken')?.value);
@@ -34,28 +35,20 @@ export default async function Profile() {
                         <label>
                             <Image src={user.avatar} width={36} height={36} alt="User's avatar" />
                             <div className={o.dynamic}>
-                                <span>Change your avatar</span>
+                                <span>{lang.getProp('avatar-btn')}</span>
                                 <input type="file" accept="image/*" />
                             </div>
                         </label>
                         <p>
-                            Avatar
-                            <span>An image shown to users on all our services. Helps recognizing you among your friends and colleagues.</span>
+                            {lang.getProp('avatar-h1')}
+                            <span>{lang.getProp('avatar-p')}</span>
                         </p>
                     </div>
-                    <Databox
-                        id="id"
-                        title="Unique identifier"
-                        description="Special identifier that is linked to your account. You can't modify it. Useful for developers."
-                        type="text"
-                        readOnly={true}
-                        value={user.id}
-                        required={true}
-                    />
+                    <Databox id="id" title={lang.getProp('uid-h1')} description={lang.getProp('uid-p')} type="text" readOnly={true} value={user.id} required={true} />
                     <Databox
                         id="username"
-                        title="Username"
-                        description="Username that is displayed on all our services and is shown to other users."
+                        title={lang.getProp('username-h1')}
+                        description={lang.getProp('username-p')}
                         type="text"
                         placeholder="..."
                         value={user.username}
@@ -63,38 +56,63 @@ export default async function Profile() {
                     />
                     <Databox
                         id="website"
-                        title="Website"
-                        description="Optional website field. A custom link that is placed below about me section on your profile."
+                        title={lang.getProp('website-h1')}
+                        description={lang.getProp('website-p')}
                         type="url"
                         placeholder="https://nove.team"
                         value={user.website}
                     />
-                    <Databox
-                        id="bio"
-                        title="About me"
-                        description="Optional about me field. Personalized space where you can write your thoughts you want to share with the world."
-                        type="textarea"
-                        placeholder="..."
-                        value={user.bio}
-                    />
-                    <Databox
-                        id="profilePublic"
-                        title="Public profile"
-                        description="Display more details (language, about me, website) on your public profile. OAuth2 apps are able (with proper permissions) to access this data no matter the setting."
-                        type="switch"
-                        checked={user.profilePublic}
-                    />
+                    <Databox id="bio" title={lang.getProp('bio-h1')} description={lang.getProp('bio-p')} type="textarea" placeholder="..." value={user.bio} />
+                    <Databox id="profilePublic" title={lang.getProp('profile-view-h1')} description={lang.getProp('profile-view-p')} type="switch" checked={user.profilePublic} />
                     <div className={o.buttons}>
                         <button type="submit" className={'btn ' + o.primary}>
-                            Save changes
+                            {lang.getCustomProp('modules.actions.save')}
                         </button>
                         <button type="reset" className="btn">
-                            Cancel
+                            {lang.getCustomProp('modules.actions.cancel')}
                         </button>
                     </div>
                 </form>
                 <aside>
                     <h1>Profile preview</h1>
+                    <div className={o.profile}>
+                        <Image src={user.avatar} width={52} height={52} alt="User's avatar" />
+                        <h1>
+                            {user.username}
+                            {user.profilePublic && (
+                                <ReactCountryFlag
+                                    countryCode={user.language.split('-')[1]}
+                                    style={{
+                                        fontSize: '16px',
+                                    }}
+                                />
+                            )}
+                        </h1>
+                        {user.profilePublic && (
+                            <>
+                                {user.bio.length && (
+                                    <div className={o.section}>
+                                        <h2>About me</h2>
+                                        <p>{user.bio}</p>
+                                    </div>
+                                )}
+                                {user.website.length && (
+                                    <div className={o.section}>
+                                        <h2>Website</h2>
+                                        <a href={user.website} rel="nofollow noopener noreferrer">
+                                            {new URL(user.website).hostname + (new URL(user.website).pathname !== '/' ? new URL(user.website).pathname : '')}
+                                        </a>
+                                    </div>
+                                )}
+                            </>
+                        )}
+                        <div className={o.footer}>
+                            {user.profilePublic && (
+                                <span>Created {new Date(user.createdAt).toLocaleDateString(user.language, { year: 'numeric', month: 'short', day: 'numeric' })}</span>
+                            )}
+                            <span>#{user.id}</span>
+                        </div>
+                    </div>
                 </aside>
             </div>
         </div>
