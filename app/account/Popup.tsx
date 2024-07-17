f1288b17de7913993617782f10b47616e6df213f
wnm210 <matt@nove.team>
2024-07-17 17:56:52 +0200

    feat: large/small popup selection

--- app/account/Popup.tsx
+++ app/account/Popup.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
 import o from './Popup.module.sass';
 
-export default function Popup({ title, description, d, children }: { title: string; description: string; d: string; children: React.ReactNode }) {
+export default function Popup({ title, description, d, type, children }: { title: string; description: string; d: string; type?: 'small' | 'large'; children: React.ReactNode }) {
     return (
-        <div className={o.popup}>
+        <div className={o.popup + (type === 'large' ? ` ${o.large}` : '')}>
+            <Link href="?et=cancel" className={o.close}></Link>
             <div className={o.container}>
                 <svg className={o.header} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="28" height="28" viewBox="0 0 24 24">
                     <path fill="currentColor" d={d} />
