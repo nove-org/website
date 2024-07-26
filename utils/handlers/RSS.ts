5323cb0a8151b1bc91f39c07f04a840e9cd3265f
wnm210 <matt@nove.team>
2024-07-26 17:04:14 +0200

    fix: parse COOKIE_HOSTNAME for rss feed

--- utils/handlers/RSS.ts
+++ utils/handlers/RSS.ts
@@ -7,7 +7,7 @@ export default class RSSHandler {
 
     constructor() {
         this.feed = new RSS({
-            title: 'Blog | Nove',
+            title: 'Blog - Nove',
             description: 'Latest information about product releases and improvements.',
             site_url: `https://${COOKIE_HOSTNAME}`,
             feed_url: `https://${COOKIE_HOSTNAME}/blog/rss.xml`,
@@ -22,12 +22,13 @@ export default class RSSHandler {
         const tagPattern = /<[^>]*?>/g;
         const brPattern = /<br\s*\/?>/gi;
         const urlPattern = /<a\s+(?:[^>]*?\s+)?href="([^"]*)"[^>]*>([^<]*)<\/a>/gi;
+        const host = COOKIE_HOSTNAME.startsWith('.') ? COOKIE_HOSTNAME.slice(1) : COOKIE_HOSTNAME;
 
         posts.forEach((post) => {
             this.feed?.item({
                 title: post.title,
                 description: post.text.replace(brPattern, '\n').replace(urlPattern, '$2 ($1) ').replace(tagPattern, ''),
-                url: `https://${COOKIE_HOSTNAME}/blog/${post.id}`,
+                url: `https://${host}/blog/${post.id}`,
                 author: post.authorUsername,
                 date: post.createdAt,
             });
