4db3b5e9e4176ab833888e8533221ddce6b1f170
wnm210 <matt@nove.team>
2024-08-17 14:38:14 +0200

    feat: create account with browser language as default

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -62,6 +62,7 @@ export default async function Register({ searchParams }: { searchParams: { [key:
                 email,
                 username,
                 password,
+                language: headers().get('Accept-Language')?.split(',')[0] || 'en-US',
             },
         });
 
